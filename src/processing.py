e4385d99d9f3d92321558fd96b2b0e29a708d24d
Ilia Ryutin <iryutin@mail.ru>
2024-12-04 21:31:10 +0300

    Дополнен файл README.md и проведена проверка по оформлению

--- src/processing.py
+++ src/processing.py
@@ -1,13 +1,12 @@
-def filter_by_state (addres_list: list, state_value: str) -> list:
+def filter_by_state(addres_list: list, state_value: str) -> list:
     """Функция фильтровки по ключю state"""
     new_addres_list = []
     for index_list, value_list in enumerate(addres_list):
-        if value_list['state'] == state_value:
+        if value_list["state"] == state_value:
             new_addres_list.append(value_list)
     return new_addres_list
 
 
-def sort_by_date (data_list:list, reverse_key:bool = True ) -> list:
+def sort_by_date(data_list: list, reverse_key: bool = True) -> list:
     """Функция сортировки данных"""
-    return sorted( data_list, key= lambda x: x['date'], reverse = reverse_key)
-
+    return sorted(data_list, key=lambda x: x["date"], reverse=reverse_key)
