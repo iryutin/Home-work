a10e8436f456edc9ae4052bad5079755a3692c59
Ilia Ryutin <iryutin@mail.ru>
2024-12-04 21:11:08 +0300

    Добавленна функция sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -6,3 +6,8 @@ def filter_by_state (addres_list: list, state_value: str) -> list:
             new_addres_list.append(value_list)
     return new_addres_list
 
+
+def sort_by_date (data_list:list, reverse_key:bool = True ) -> list:
+    """Функция сортировки данных"""
+    return sorted( data_list, key= lambda x: x['date'], reverse = reverse_key)
+
