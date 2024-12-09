e9a6c73bfa1b7ddae0a8a7aecd2e1e761c3af669
Ilia Ryutin <iryutin@mail.ru>
2024-12-09 22:02:37 +0300

    Добавлен тест для  filter_by_state, get_mask_account, и оптимизация по мелочи.

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,14 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция маскировки номера карты"""
-    return f'{card_number[0:4]} {card_number[4:6]}** ****'
+    if card_number.isdigit() == True and len(card_number) == 12:
+        return f'{card_number[0:4]} {card_number[4:6]}** ****'
+    else:
+        return "Не корректные данные"
 
 
 def get_mask_account(account: str) -> str:
-    """Функция  маскировки номера счёта"""
-    return "**" + account[-5:-1]
+    """Функция маскировки номера счёта"""
+    if account.isdigit() == True and len(account) == 20:
+        return "**" + account[-4:]
+    else:
+        return "Не корректные данные"
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,4 +1,13 @@
 from src.masks import get_mask_card_number
+from src.masks import get_mask_account
+import pytest
 
-def test_get_mask_card_number ():
-   assert get_mask_card_number('123456789123') == '1234 56** ****'
+@pytest.mark.parametrize('card_input, card_output',[('123456789123','1234 56** ****'),('123','Не корректные данные')])
+
+def test_get_mask_card_number (card_input, card_output):
+   assert get_mask_card_number(card_input) == card_output
+
+@pytest.mark.parametrize('account_input, account_output',[('73654108430135874305','**4305'),('123456789123','Не корректные данные')])
+
+def test_get_mask_account (account_input, account_output):
+   assert get_mask_account(account_input) == account_output
