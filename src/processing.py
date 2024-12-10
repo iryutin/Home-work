39704b22fe21d49ed769c2da77be3399f0b1b6c6
Ilia Ryutin <iryutin@mail.ru>
2024-12-10 18:51:23 +0300

    Добавлен тест для test_sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(addres_list: list, state_value: str) -> list:
+def filter_by_state(addres_list: list, state_value: str = 'EXECUTED') -> list:
     """Функция фильтровки по ключю state"""
     new_addres_list = []
     for index_list, value_list in enumerate(addres_list):
@@ -12,4 +12,4 @@ def filter_by_state(addres_list: list, state_value: str) -> list:
 
 def sort_by_date(data_list: list, reverse_key: bool = True) -> list:
     """Функция сортировки данных"""
-    return sorted(data_list, key=lambda x: x["date"], reverse=reverse_key)
+    return sorted(data_list, key=lambda x: x.get("date"), reverse=reverse_key)
