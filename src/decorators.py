7316f7f2813994812e927af8793a7919a43c0e5b
Ilia Ryutin <iryutin@mail.ru>
2024-12-22 14:43:18 +0300

    Вроде всё по красоте

--- src/decorators.py
+++ src/decorators.py
@@ -1,23 +1,24 @@
-def log(filename: str = ''):
+from typing import Callable, Any
+
+def log(filename: str = "") -> Callable[[str], Any]:
     """Декоратор логирования функции на ошибку или нет, создаёт файл если указать имя файла"""
-    def decorator_log(func):
-        def wrapper(*args, **kwargs):
+    def decorator_log(func: Any) -> Callable[[Any], Any]:
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
             try:
                 result = func(*args, **kwargs)
             except Exception as error_type:
-                if filename != '':
-                    with open(f'{filename}.txt','w') as file_log:
-                        file_log.write(f'{func.__name__} error: {error_type}. Inputs: {args}, {kwargs}')
+                if filename != "":
+                    with open(f"{filename}.txt", "w") as file_log:
+                        file_log.write(f"{func.__name__} error: {str(error_type)}. Inputs: {args}, {kwargs}")
                 else:
-                    print(f'{func.__name__} error: {error_type}. Inputs: {args}, {kwargs}')
+                    print(f"{func.__name__} error: {str(error_type)}. Inputs: {args}, {kwargs}")
             else:
-                if filename != '':
-                    with open(f'{filename}.txt','w') as file_log:
-                        file_log.write(f'{func.__name__} ok')
+                if filename != "":
+                    with open(f"{filename}.txt", "w") as file_log:
+                        file_log.write(f"{func.__name__} ok")
                     return result
                 else:
-                    print(f'{func.__name__} ok')
+                    print(f"{func.__name__} ok")
                     return result
         return wrapper
     return decorator_log
-
