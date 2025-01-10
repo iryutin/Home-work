e6580195496e0116893e3bae0835ef501e6dc11a
Ilia Ryutin <iryutin@mail.ru>
2025-01-10 22:45:53 +0300

    Добавил чтение csv, excel пофиксил логи.

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler(f"logs/{__name__}.log")
+file_handler = logging.FileHandler(f"D:/my_project2/pythonProject1/logs/{__name__}.log")
 file_formater = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formater)
 logger.addHandler(file_handler)
