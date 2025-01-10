2bbe38f0dff2f02a626a31b057bfa05fa9e364cc
Ilia Ryutin <iryutin@mail.ru>
2025-01-10 22:57:16 +0300

    теперь точно пофиксил логи.

--- src/file_reader.py
+++ src/file_reader.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import logging
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('file_reader')
 logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler(f"D:/my_project2/pythonProject1/logs/{__name__}.log")
+file_handler = logging.FileHandler(f"D:/my_project2/pythonProject1/logs/file_reader.log")
 file_formater = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formater)
 logger.addHandler(file_handler)
